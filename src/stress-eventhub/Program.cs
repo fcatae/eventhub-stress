7e408f97b5067a7ca9e534b9ce4afa00b3afa41a
Fabricio Catae <fcatae@microsoft.com>
2016-07-05 18:48:33 -0300

    test event hub

--- src/stress-eventhub/Program.cs
+++ src/stress-eventhub/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.ServiceBus.Messaging;
 
 namespace stress_eventhub
 {
@@ -9,7 +12,34 @@ namespace stress_eventhub
     {
         public static void Main(string[] args)
         {
+            Stopwatch watch = new Stopwatch();
+
             Console.WriteLine("Hello world");
+
+            var builder = new ConfigurationBuilder()
+                .AddEnvironmentVariables()
+                .AddUserSecrets("aspnet-stress-eventhub-20160705064558");
+
+            var config = builder.Build();
+
+            Publisher pub = new Publisher(config["eventhub"]);
+
+            Console.Write("Initializing... ");
+
+            pub.InitAsync().Wait();
+            Console.WriteLine("DONE");
+
+            watch.Start();
+
+            Console.Write("Sending messages... ");
+            pub.SendAsync().Wait();
+            Console.WriteLine("DONE");
+
+            watch.Stop();
+
+            Console.WriteLine();
+            Console.WriteLine("  Total time = {0}", watch.ElapsedMilliseconds);
+
             Console.ReadLine();
         }
     }
---
+++ src/stress-eventhub/Publisher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace stress_eventhub
+{
+    public class Publisher
+    {
+        EventHubClient _client;
+
+        public Publisher(string connStr)
+        {
+            _client = EventHubClient.CreateFromConnectionString(connStr);
+        }
+
+        public Task InitAsync()
+        {
+            return _client.SendAsync(new EventData(Encoding.UTF8.GetBytes("Init")));
+        }
+
+        public Task SendAsync()
+        {
+            int batchSize = 10;
+            byte[] messageBody = Encoding.UTF8.GetBytes("Send async message");
+
+            EventData[] eventList = new EventData[batchSize];
+
+            for (int i=0; i<batchSize; i++)
+            {
+                eventList[i] = new EventData(messageBody);
+            }
+
+            return _client.SendBatchAsync(eventList);
+        } 
+    }
+}
